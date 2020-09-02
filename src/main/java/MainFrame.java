97100ede2c5d1e6bc3ee74f8342fdbcbfed33d70
joshpetit <joshuapetitma@yahoo.com>
2020-09-02 07:51:11 -0400

    feat: made frame draggable

--- src/main/java/MainFrame.java
+++ src/main/java/MainFrame.java
@@ -1,9 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class MainFrame extends JFrame {
+    int posX, posY;
     public MainFrame() {
         super();
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -12,12 +14,25 @@ public class MainFrame extends JFrame {
         this.setAlwaysOnTop(true);
         GridBagLayout layout = new GridBagLayout();
         this.setLayout(layout);
-
+        this.setBounds(0,0,150,75);
+        this.addMouseListener(new MouseAdapter(){
+            @Override
+            public void mousePressed(MouseEvent mouseEvent) {
+                posX = mouseEvent.getX();
+                posY = mouseEvent.getY();
+            }
+        });
+        this.addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e){
+                setLocation(e.getXOnScreen()-posX,e.getYOnScreen() - posY);
+            }
+        });
         JLabel time = new JLabel();
         TimeChange tc = time::setText;
         Time timeChange = new Time(tc);
-
         this.add(time);
         this.setVisible(true);
     }
+
 }
