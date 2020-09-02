7fdbae206d43f79f4a1690a05eb740b530409172
joshpetit <joshuapetitma@yahoo.com>
2020-09-02 09:44:22 -0400

    feat: resize with ctrl + move mouse up or down

--- src/main/java/MainFrame.java
+++ src/main/java/MainFrame.java
@@ -2,10 +2,11 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 public class MainFrame extends JFrame {
     int posX, posY;
+    JLabel time;
+
     public MainFrame() {
         super();
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -14,8 +15,8 @@ public class MainFrame extends JFrame {
         this.setAlwaysOnTop(true);
         GridBagLayout layout = new GridBagLayout();
         this.setLayout(layout);
-        this.setBounds(0,0,150,75);
-        this.addMouseListener(new MouseAdapter(){
+        this.setBounds(0, 0, 150, 75);
+        this.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent mouseEvent) {
                 posX = mouseEvent.getX();
@@ -23,16 +24,31 @@ public class MainFrame extends JFrame {
             }
         });
         this.addMouseMotionListener(new MouseAdapter() {
+            int lastY;
+
             @Override
-            public void mouseDragged(MouseEvent e){
-                setLocation(e.getXOnScreen()-posX,e.getYOnScreen() - posY);
+            public void mouseDragged(MouseEvent e) {
+                if (e.isControlDown()) {
+                    if (lastY < e.getYOnScreen()) {
+                        setSize(MainFrame.this.getWidth() - 1, MainFrame.this.getHeight() - 1);
+                    } else {
+                        setSize(MainFrame.this.getWidth() + 1, MainFrame.this.getHeight() + 1);
+                    }
+                    lastY = e.getYOnScreen();
+                } else {
+                    setLocation(e.getXOnScreen() - posX, e.getYOnScreen() - posY);
+                }
             }
         });
-        JLabel time = new JLabel();
+        time = new JLabel();
         TimeChange tc = time::setText;
         Time timeChange = new Time(tc);
         this.add(time);
         this.setVisible(true);
+    }
+
+    private void incFontSize() {
+        time.setFont(new Font(Font.SERIF, Font.PLAIN, time.getFont().getSize() + 1));
     }
 
 }
